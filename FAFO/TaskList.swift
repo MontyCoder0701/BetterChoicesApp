feb0d77fe3dc6d1ee2cb459a94f19bc9829a91c0
Soo <soojlee0106@naver.com>
2023-11-26 18:01:07 +0900

    Add local storage

--- FAFO/TaskList.swift
+++ FAFO/TaskList.swift
@@ -10,12 +10,44 @@ import Foundation
 class TaskList: ObservableObject {
     @Published var tasks: [Task] = []
     
+    init() {
+        loadTasks()
+    }
+    
     func handleAddTask(name: String) {
+        if (name.isEmpty) {
+            return;
+        }
+        
         let task = Task(name: name)
         tasks.append(task)
+        saveTasks()
     }
     
     func handleRemoveTask(at index: IndexSet) {
         tasks.remove(atOffsets: index)
+        saveTasks()
     }
+    
+    private func saveTasks() {
+        do {
+            let encoder = JSONEncoder()
+            let data = try encoder.encode(tasks)
+            UserDefaults.standard.set(data, forKey: "tasks")
+        } catch {
+            print("Error encoding tasks")
+        }
+    }
+
+    
+    private func loadTasks() {
+            if let data = UserDefaults.standard.data(forKey: "tasks") {
+                do {
+                    let decoder = JSONDecoder()
+                    tasks = try decoder.decode([Task].self, from: data)
+                } catch {
+                    print("Error decoding tasks")
+                }
+            }
+        }
 }
