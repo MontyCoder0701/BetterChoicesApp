72f6dbc4c0b902e926825f2b37d0ff2d25e802ec
Soo <soojlee0106@naver.com>
2023-11-26 13:09:49 +0900

    initial commit

---
+++ FAFO/FAFOApp.swift
@@ -0,0 +1,17 @@
+//
+//  FAFOApp.swift
+//  FAFO
+//
+//  Created by Soojeong Lee on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct FAFOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
